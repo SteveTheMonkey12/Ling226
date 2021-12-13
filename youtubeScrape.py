83d1a7ee07b814d4bc9c4f00d4953e9b6c72701b
Elliott Ramoo <ewramo@gmail.com>
2021-12-13 14:51:24 +1300

    first bash at multithread

--- youtubeScrape.py
+++ youtubeScrape.py
@@ -1,18 +1,9 @@
 from youtube_transcript_api import YouTubeTranscriptApi as YTA
 from youtube_transcript_api.formatters import JSONFormatter
 import pafy
-
-def main():
-    #url  = "https://www.youtube.com/watch?v=GFKHOiufjkI&list=UUxyCzPY2pjAjrxoSYclpuLg"
-    #videos = pafy.get_playlist2(url)
-    with open("videos.txt", "r") as videos:
-        video_ids = videos.readlines()
-    video_num = 0
-    num_videos = len(video_ids)
-    formatter = JSONFormatter()
-    for video_id in video_ids:
-        video_num+=1
-        try:
+import threading
+def thread_function(video_id, video_num, num_videos):
+    try:
             transcript = YTA.get_transcript(video_id)
             # .format_transcript(transcript) turns the transcript into a JSON string.
             json_formatted = formatter.format_transcript(transcript)
@@ -32,12 +23,29 @@ def main():
         try:
             with open("./output/" + title +".json", 'w', encoding='utf-8') as json_file:
                 json_file.write(json_formatted)
-        
+
             print(f'{(video_num/num_videos)*100:.2f}% {title} {video_id}')
         except:
             print("file name invalid" + title)
             with open("fileWriteError.txt", 'a') as f:
                 f.write(video_id + " " + title)
+
+def main():
+    #url  = "https://www.youtube.com/watch?v=GFKHOiufjkI&list=UUxyCzPY2pjAjrxoSYclpuLg"
+    #videos = pafy.get_playlist2(url)
+    with open("videos.txt", "r") as videos:
+        video_ids = videos.readlines()
+    video_num = 0
+    num_videos = len(video_ids)
+    formatter = JSONFormatter()
+    threads = list()
+    for video_id in video_ids:
+        video_num+=1
+        x = threading.Thread(target=thread_function, args=(video_id, video_num, num_videos,))
+        threads.append(x)
+        x.start()
+    for index, thread in enumerate(threads):
+        thread.join()
        
 if __name__ == "__main__":
     main()
